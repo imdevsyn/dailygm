a48e1ca8f408bc48e83afe96745ad5b92fe519d6
imdevsyn <imdevsyn@gmail.com>
2024-10-12 11:13:12 -0300

    resolve fallback font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['var(--font-family-poppins)']
+        sans: ['var(--font-family-poppins)', 'sans-serif']
       },
       keyframes: {
         "accordion-down": {
